a73f3ea583a29e1e909fa077e68ec2407b295eca
Mayuki Yamabe <myamabe@amazon.com>
2020-12-08 22:24:25 +0900

    add output attributes

--- outputs.tf
+++ outputs.tf
@@ -17,3 +17,18 @@ output "private_subnet_1_id" {
 output "private_subnet_2_id" {
   value = aws_subnet.private_subnet_1.id
 }
+output "vpc_cidr" {
+  value = aws_vpc.this.cidr_block
+}
+output "public_subnet_1_route_table_id" {
+  value = aws_route_table.public_subnet_1_route_table.id
+}
+output "public_subnet_2_route_table_id" {
+  value = aws_route_table.public_subnet_2_route_table.id
+}
+output "private_subnet_1_route_table_id" {
+  value = aws_route_table.private_subnet_1_route_table.id
+}
+output "private_subnet_2_route_table_id" {
+  value = aws_route_table.private_subnet_2_route_table.id
+}
